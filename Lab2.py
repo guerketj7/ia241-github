cd1752de32824d76d695a7ba39c9f69e6d9c3f6a
EC2 Default User <ec2-user@ip-172-31-32-198.ec2.internal>
2022-01-26 21:39:27 +0000

    lec2

---
+++ Lab2.py
@@ -0,0 +1,51 @@
+"""
+
+This is a regional comment
+
+Lec2
+
+"""
+
+#print('hello World') #This is a comment
+
+#print(type(123))
+
+#print(type(123.))
+
+#print(type("123"))
+
+#print("hello world")
+#print('hello world')
+
+#print("Toms's cat")
+
+#print('hello world')
+
+#print('hello world'.upper())
+#print('hello world'.lower())
+
+#print('  hello world  ')
+#print('  hello world.  '.strip())
+
+#print('hello world'.split())
+#print('hello, world'.split(','))
+
+#print('hello, world'.split('o'))
+
+#print (2. ** 3)
+
+my_int = 123
+
+print(my_int)
+
+my_int = 321
+
+print(my_int)
+
+my_float = 123.
+
+my_str = 'hello world'
+
+print(my_float + 1)
+
+print(my_str.upper())
