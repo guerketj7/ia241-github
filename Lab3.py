36e063b46b5987b49ac6d29d4e4fbc8961d855f7
EC2 Default User <ec2-user@ip-172-31-32-198.ec2.internal>
2022-02-07 21:24:08 +0000

    lab3

--- Lab3.py
+++ Lab3.py
@@ -25,3 +25,25 @@ my_list = ['a', '123', 123, 'b', 'B', 'False', False, 123, None , 'none']
 print(len(set(my_list)))
 
 #3.6
+print(len("This is my third python lab.".split()))
+
+#3.7 
+num_list = [12, 32, 43, 35]
+num_list.sort()
+print(num_list)
+#print(min(num_list))
+print(num_list[0])
+print(num_list[-1])
+
+#3.8 
+game_board = [[0,0,0],
+              [0,0,0],
+              [0,0,0]]
+'''
+what we want to get: 
+                [[0,0,0]
+                 [0,1,0]
+                 [0,0,0]]
+'''
+game_board[1][1] = 1 
+print(game_board)
