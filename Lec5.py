be383d076c0ccc25f565aa8a3b74d11ab0e3e642
EC2 Default User <ec2-user@ip-172-31-32-198.ec2.internal>
2022-02-21 21:25:45 +0000

    Lec5.py

---
+++ Lec5.py
@@ -0,0 +1,51 @@
+'''
+lec 5 if statment 
+'''
+
+import this
+
+print( 2+
+       1)
+       
+print([ 1,2,3,
+        4,5,6])
+        
+m = 1 +\
+        2
+print(m)
+
+a=[1,2,3]
+b = [1,2,3]
+
+print(id([1,2,3]))
+print(id(a))
+print(id(b))
+
+print(a==b)
+print(a is b)
+
+x = None
+
+print(id(None))
+print(id(x))
+
+print( x == None)
+print(x is None)
+
+y = []
+print(y == None)
+print(y is None)
+
+print(True and False)
+print(True or False)
+print( not True)
+print(not False)
+
+print (not None)
+print(not '0')
+
+print(() and [])
+print([] and ())
+
+print(-1 or 0 )
+print(0 or -1)
