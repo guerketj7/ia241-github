1ef95e68f15da9a0b33499e5789a89f82ce3b5fa
EC2 Default User <ec2-user@ip-172-31-32-198.ec2.internal>
2022-03-07 20:34:23 +0000

    Lab6.py

--- Lec5.py
+++ Lec5.py
@@ -43,9 +43,30 @@ print(not False)
 
 print (not None)
 print(not '0')
-
+'''
 print(() and [])
 print([] and ())
 
 print(-1 or 0 )
 print(0 or -1)
+'''
+if 2>1 : 
+    print('2>1')
+    if 3>1 :
+        print('3>1')
+        
+    if 2<1:
+        print('2<=1')
+        
+    print('not in the if block')
+
+if 2<=1:
+     print('2<=1')
+else: 
+     print('2 > 1')
+        
+if 2<=2:
+    print('2<=2')
+else: 
+    print('2>2')
+        
